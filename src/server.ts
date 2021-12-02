adcca5dbe7eb629f00ba9e5bf404c5eb484d4dda
Christopher Hallett <chrisjameshal@gmail.com>
2021-12-02 11:37:52 +0000

    Alter connection string

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,12 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-const client = new Client({ database: "toDoList" });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 client.connect();
 
 app.use(express.json());
